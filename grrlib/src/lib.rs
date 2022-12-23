e1b6d544d5b0191871886cbd462714fdbf44ea39
Luc van den Brand <lucvandenbrand@protonmail.com>
2022-12-23 10:33:22 +0100

    Restructure.

---
+++ grrlib/src/lib.rs
@@ -0,0 +1,10 @@
+#![allow(non_upper_case_globals)]
+#![allow(non_camel_case_types)]
+#![allow(non_snake_case)]
+#![allow(unaligned_references)]
+#![no_std]
+
+include!("grrlib.rs");
+
+mod inline;
+pub use inline::*;
